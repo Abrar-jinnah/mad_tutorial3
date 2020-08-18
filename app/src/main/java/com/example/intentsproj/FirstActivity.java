20586049f12cb1c7428e7c4dcd32dfc2239cd2f3
Abrar-jinnah <abrarjinnah@gmail.com>
2020-08-19 00:25:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -0,0 +1,56 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    EditText etnum1;
+    EditText etnum2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        //Define text objects
+        etnum1 = findViewById(R.id.etnum1);
+        etnum2 = findViewById(R.id.etnum2);
+
+    }
+
+    public void openSecond(View view){
+        Intent intent=new Intent(this,SecondActivity.class);
+        intent.putExtra("textView_num1",etnum1.getText().toString());
+        intent.putExtra("textView_num2",etnum2.getText().toString());
+
+
+
+
+
+        LayoutInflater li = getLayoutInflater();
+
+        View layout = li.inflate(R.layout.custom_toast, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);
+        toast.show();
+
+        startActivity(intent);
+    }
+
+}
